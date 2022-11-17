1d196f6747b6d2833d77719976aae76fabe09a64
VIGNESH BABU B R B <20576119+vigneshbrb@users.noreply.github.com>
2022-11-17 22:47:52 +0530

    Initial Commit

---
+++ src/main/java/com/brb/springboot/JavaWebAppWorkshopApplication.java
@@ -0,0 +1,13 @@
+package com.brb.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaWebAppWorkshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaWebAppWorkshopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/brb/springboot/JavaWebAppWorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brb.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaWebAppWorkshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
